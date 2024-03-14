aa14fde871a96c36f9e5cfd1a5238a3dd3df8ec9
leekc1715 <162955207+leekc1715@users.noreply.github.com>
2024-03-14 09:01:58 +0900

    feat: converting

---
+++ routes/sqlserver.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+const sqlConfig = require('../config/sqlserver.config');
+
+module.exports = router;
---
+++ test.js
@@ -0,0 +1,112 @@
+const sql = require('mssql');
+const mysql = require('mysql2/promise');
+
+const sqlConfig = {
+    user: 'sa',
+    password: '@1023ldde1023',
+    server: 'DESKTOP-VI1EG1F',
+    database: 'Almighty',
+    options: {
+        encrypt: false, // 필요에 따라 수정
+      },
+    port: 1433,
+}
+
+const mysqlConfig = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '0000',
+    database: 'h00044'
+}
+
+// 현재시간을 나타내는 코드
+function getCurrentDateTimeString() {
+    const currentDate = new Date();
+  
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+  
+    const formattedDateTime = `${year}${month}${day}${hours}${minutes}${seconds}`;
+  
+    return formattedDateTime;
+  }
+
+
+async function getfetchCustomer() {
+    try {
+      // sqlserver 연결
+      await sql.connect(sqlConfig);
+  
+       // 쿼리 실행
+       const result = await sql.query('SELECT * FROM CUST_INFO');
+
+       return result.recordset;
+
+    } catch (err) {
+      console.error(err);
+    } finally {
+      // 연결 종료
+      sql.close();
+    }
+  }
+
+async function customerData(glovalValue) {
+    const currentDateTimeString = getCurrentDateTimeString();
+  try{
+      // mysql 연결
+      const connection = await mysql.createConnection(mysqlConfig);
+
+      glovalValue.map(async (data) => {
+        const query = `
+        INSERT INTO tcustomerpersonal (
+          CUSTOMERID, CUSTNAME, CUSTJN, CUSTDOB, DOBTYPE, CUSTGENDER,
+          CUSTREGDATE, CUSTFSTDATE, CUSTADDR11, CRTIME
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `;
+
+        let ctznno = data.CTZN_NO.slice(0, 6);
+        let ctznnoStation = Number(data.CTZN_NO.slice(0, 2));
+        if (ctznnoStation > 24) {
+            ctznno = '19' + ctznno;
+        }else {
+            ctznno = '20' + ctznno;
+        }
+        if ( data.SEX === 'F') {
+            data.SEX = 2;
+        }else {
+            data.SEX = 1;
+        }
+
+        const values = [data.CUST_NO, data.NAME, data.CTZN_NO,ctznno, 0, data.SEX, data.ENTR_DAY, data.MDFY_DAY, data.ADDR1 + data.ADDR2, currentDateTimeString]
+            
+        const [rows, fields] = await connection.execute(query, values);
+
+        console.log(`Inserted CUSTOMERID: ${data.CUST_NO}`);
+      })
+
+      // 연결 종료
+      await connection.end()
+  } catch(err) {
+      console.error(err);
+  }
+  
+}
+
+
+async function main() {
+    try {
+        // fetchData 함수 호출 후 결과 확인
+        const glovalValue = await getfetchCustomer();
+    
+        // customerData 함수 호출
+        await customerData(glovalValue);
+      } catch (error) {
+        console.error(error);
+      }
+}
+
+main();
